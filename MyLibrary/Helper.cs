ab60fc07e3bc626d18a7c6364fba4dffa3ae8248
lliang826 <xlawrence.liangx@gmail.com>
2022-09-08 14:43:52 -0700

    initial commit

---
+++ MyLibrary/Helper.cs
@@ -0,0 +1,11 @@
+namespace MyLibrary;
+
+public class Helper
+{
+    public static string Reverse(string s)
+    {
+        char[] charArray = s.ToCharArray();
+        Array.Reverse(charArray);
+        return new string(charArray);
+    }
+}
---
+++ ReverseText/Program.cs
@@ -0,0 +1,10 @@
+﻿using MyLibrary;
+
+public class Booboo {
+	public static void Main(String[] args) {
+		Console.Write("Please enter your name: ");
+		String? name = Console.ReadLine();
+
+		Console.WriteLine(Helper.Reverse(name!));
+	}GCNotificationStatus
+}
